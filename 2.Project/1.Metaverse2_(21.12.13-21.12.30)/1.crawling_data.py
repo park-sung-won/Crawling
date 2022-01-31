80703e54f85343630a567523e1c554daca17d9ab
footfoot <93960623+park-sung-won@users.noreply.github.com>
2022-01-31 17:33:45 +0900

    Update 1.crawling_data.py

--- 2.Project/1.Metaverse2_(21.12.13-21.12.30)/1.crawling_data.py
+++ 2.Project/1.Metaverse2_(21.12.13-21.12.30)/1.crawling_data.py
@@ -49,3 +49,29 @@ df_final = pd.concat(df_all).reset_index()
 df_final.to_csv("./meta_data_all.csv")
 
 driver.quit()
+
+
+
+######
+
+import pandas as pd
+from tqdm import trange
+import time
+import requests
+
+def metalist(page_no): 
+    url='https://api.metaverse2.com/api/v1.0/order/market_list'
+    params=f'lastPage=5283&page={page_no}&sort=totalPrice&order=DESC'
+    response=requests.get(url,params=params)
+    metalist=response.json()
+    return pd.DataFrame(metalist)
+
+metabus2=[]
+
+for page_no in trange(1, 마지막페이지수+1):
+    temp=metalist(page_no)
+    metabus2.append(temp)
+    time.sleep(0.01)
+    
+metabus2_ = pd.concat(metabus2)
+metabus2_
